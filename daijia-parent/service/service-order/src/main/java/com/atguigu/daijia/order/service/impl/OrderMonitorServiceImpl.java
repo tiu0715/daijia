8089394a331ab5cb47315aa4a8b9a049986b6709
hz002 <1150999751@qq.com>
2024-09-03 16:21:21 +0800

    保存订单监控信息 上传录音

--- daijia-parent/service/service-order/src/main/java/com/atguigu/daijia/order/service/impl/OrderMonitorServiceImpl.java
+++ daijia-parent/service/service-order/src/main/java/com/atguigu/daijia/order/service/impl/OrderMonitorServiceImpl.java
@@ -1,7 +1,9 @@
 package com.atguigu.daijia.order.service.impl;
 
 import com.atguigu.daijia.model.entity.order.OrderMonitor;
+import com.atguigu.daijia.model.entity.order.OrderMonitorRecord;
 import com.atguigu.daijia.order.mapper.OrderMonitorMapper;
+import com.atguigu.daijia.order.repository.OrderMonitorRecordRepository;
 import com.atguigu.daijia.order.service.OrderMonitorService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,15 @@ import org.springframework.stereotype.Service;
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class OrderMonitorServiceImpl extends ServiceImpl<OrderMonitorMapper, OrderMonitor> implements OrderMonitorService {
+    /**
+     * 保存订单监控记录数据
+     */
+    @Autowired
+    private OrderMonitorRecordRepository orderMonitorRecordRepository;
 
-
+    @Override
+    public Boolean saveOrderMonitorRecord(OrderMonitorRecord orderMonitorRecord) {
+        orderMonitorRecordRepository.save(orderMonitorRecord);
+        return true;
+    }
 }
